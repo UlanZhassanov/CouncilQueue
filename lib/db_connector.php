ca92156d02c1a4fc78740605d156c0aa65553dec
UlanZhassanov <ulanzhasanov@gmail.com>
2023-07-14 15:14:13 +0600

    dbdb

--- lib/db_connector.php
+++ lib/db_connector.php
@@ -1,13 +1,14 @@
 <?php
 // used to connect to the database
 $host = "localhost"; //Host and Server 
-$db_name = "queue_sys_"; // database name
-$username = "root"; // username for accessing school_management-db
-$password = "";// password for accessing school_management-db
+$port = 3306;
+$db_name = "que"; // database name
+$username = "queroot"; // username for accessing school_management-db
+$password = "8Yv8xb?71";// password for accessing school_management-db
  
 /////////////////////////////////////////////////////////////////////////////////
 try {
-    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+    $con = new PDO("mysql:host={$host}:$port;dbname={$db_name}", $username, $password);
 }
 //////////////////////////////////////////////////////////////////////////////////
 
